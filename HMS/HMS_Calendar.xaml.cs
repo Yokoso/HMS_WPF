527f40e52d3e250d4ba3626b5021612af558e1f5
Tomek <tomsolga90@gmail.com>
2020-08-19 21:40:17 +0200

    Add project files.

---
+++ HMS/HMS_Calendar.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HMS
+{
+    /// <summary>
+    /// Interaction logic for HMS_Calendar.xaml
+    /// </summary>
+    public partial class HMS_Calendar : Page
+    {
+        public HMS_Calendar()
+        {
+            InitializeComponent();
+        }
+    }
+}
